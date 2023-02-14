8ebe4169f21148729424c16560dba83663c4d481
Walber Oliveira <walberjr.oliveira@gmail.com>
2023-02-14 18:03:18 -0300

    #4 - Implementing the new scenarios to the feature

---
+++ src/main/kotlin/br/com/walloliveira/resources/v1/exception/ErrorResponseData.kt
@@ -0,0 +1,3 @@
+package br.com.walloliveira.resources.v1.exception
+
+data class ErrorResponseData(val message: String?)
--- src/test/kotlin/br/com/walloliveira/CustomerConfigResourceTest.kt
+++ src/test/kotlin/br/com/walloliveira/CustomerConfigResourceTest.kt
@@ -36,4 +36,62 @@ class CustomerConfigResourceTest {
             .body("data[0].clientId", `is`("client-123"))
             .body("data[0].api", `is`("PAG_SEGURO"))
     }
+
+    @Test
+    fun newCustomerConfigShouldBeNotCreatedWhenPayloadIsInvalid() {
+        val payload = NewCustomerConfigData(
+            "",
+            "",
+            "",
+        )
+        val customerCode = UUID.randomUUID()
+        RestAssured.given()
+            .body(payload)
+            .header("Content-Type", MediaType.APPLICATION_JSON)
+            .`when`()
+            .post("/v1/customer/{customerCode}/config", customerCode)
+            .then()
+            .statusCode(400)
+    }
+
+    @Test
+    fun newCustomerConfigShouldBeNotCreatedWhenApiFieldOnPayloadIsInvalid() {
+        val payload = NewCustomerConfigData(
+            "token-123",
+            "client-321",
+            "OTHER",
+        )
+        val customerCode = UUID.randomUUID()
+        RestAssured.given()
+            .body(payload)
+            .header("Content-Type", MediaType.APPLICATION_JSON)
+            .`when`()
+            .post("/v1/customer/{customerCode}/config", customerCode)
+            .then()
+            .statusCode(400)
+    }
+
+    @Test
+    fun newCustomerConfigShouldBeNotCreatedWhenApiValueIsTheSame() {
+        val payload = NewCustomerConfigData(
+            "token-123",
+            "client-321",
+            "PAG_SEGURO",
+        )
+        val customerCode = UUID.randomUUID()
+        RestAssured.given()
+            .body(payload)
+            .header("Content-Type", MediaType.APPLICATION_JSON)
+            .`when`()
+            .post("/v1/customer/{customerCode}/config", customerCode)
+            .then()
+            .statusCode(201)
+        RestAssured.given()
+            .body(payload)
+            .header("Content-Type", MediaType.APPLICATION_JSON)
+            .`when`()
+            .post("/v1/customer/{customerCode}/config", customerCode)
+            .then()
+            .statusCode(409)
+    }
 }
